c5b0339d3ecc83e3be87e7aecb738ab1fb5d7865
Catalin Gavrila <cata_bb92@yahoo.com>
2021-04-14 18:05:22 +0300

    completed methods 2 and 3

--- script.js
+++ script.js
@@ -1,21 +1,74 @@
 
 document.getElementById("submit").addEventListener("click", calculateChances);
 function calculateChances() {
-	let mja = parseInt(document.getElementById("mja").value);
+
+	// Metoda I
+	let mja = 5;
 	let victoriiG = parseInt(document.getElementById("victoriiG").value);
 	let egaluriG = parseInt(document.getElementById("egaluriG").value);
-	let infrangeriG = parseInt(document.getElementById("infrangeriG").value);
-	let mjd = parseInt(document.getElementById("mjd").value);
+	let mjd = 5;
 	let victoriiO = parseInt(document.getElementById("victoriiO").value);
 	let egaluriO = parseInt(document.getElementById("egaluriO").value);
-	let infrangeriO = parseInt(document.getElementById("infrangeriO").value);
-	let puncteG = victoriiG * 3 + egaluriG * 1 + infrangeriG * 0;
+	let puncteG = victoriiG * 3 + egaluriG * 1;
 	let coeficientG = Math.round((puncteG / mja) * 1e2 ) / 1e2 * 100;
-	let puncteO = victoriiO * 3 + egaluriO * 1 + infrangeriO * 0;
+	let puncteO = victoriiO * 3 + egaluriO * 1;
 	let coeficientO = Math.round((puncteO / mjd) * 1e2 ) / 1e2 * 100;
-	let fj = ((coeficientG - coeficientO) * 100);
-	let fjProc = Math.round((fj / 300) * 1e2 ) / 1e2;
-	let sanseG = 50 + fjProc;
-	let sanseO = 50 - fjProc;
+	let met1 = ((coeficientG - coeficientO) * 100);
+	let met1Proc = Math.round((met1 / 300) * 1e2 ) / 1e2;
+
+	// Metoda II
+	let victorieAcasaG1 = parseInt(document.getElementById("victorieAcasaG1").value);
+	let egalAcasaG1 = parseInt(document.getElementById("egalAcasaG1").value);
+	let victorieDeplasareG1 = parseInt(document.getElementById("victorieDeplasareG1").value);
+	let egalDeplasareG1 = parseInt(document.getElementById("egalDeplasareG1").value);
+	let victorieAcasaG2 = parseInt(document.getElementById("victorieAcasaG2").value);
+	let egalAcasaG2 = parseInt(document.getElementById("egalAcasaG2").value);
+	let victorieDeplasareG2 = parseInt(document.getElementById("victorieDeplasareG2").value);
+	let egalDeplasareG2 = parseInt(document.getElementById("egalDeplasareG2").value);
+	let victorieAcasaG3 = parseInt(document.getElementById("victorieAcasaG3").value);
+	let egalAcasaG3 = parseInt(document.getElementById("egalAcasaG3").value);
+	let victorieDeplasareG3 = parseInt(document.getElementById("victorieDeplasareG3").value);
+	let egalDeplasareG3 = parseInt(document.getElementById("egalDeplasareG3").value);
+	let victorieAcasaO1 = parseInt(document.getElementById("victorieAcasaO1").value);
+	let egalAcasaO1 = parseInt(document.getElementById("egalAcasaO1").value);
+	let victorieDeplasareO1 = parseInt(document.getElementById("victorieDeplasareO1").value);
+	let egalDeplasareO1 = parseInt(document.getElementById("egalDeplasareO1").value);
+	let victorieAcasaO2 = parseInt(document.getElementById("victorieAcasaO2").value);
+	let egalAcasaO2 = parseInt(document.getElementById("egalAcasaO2").value);
+	let victorieDeplasareO2 = parseInt(document.getElementById("victorieDeplasareO2").value);
+	let egalDeplasareO2 = parseInt(document.getElementById("egalDeplasareO2").value);
+	let victorieAcasaO3 = parseInt(document.getElementById("victorieAcasaO3").value);
+	let egalAcasaO3 = parseInt(document.getElementById("egalAcasaO3").value);
+	let victorieDeplasareO3 = parseInt(document.getElementById("victorieDeplasareO3").value);
+	let egalDeplasareO3 = parseInt(document.getElementById("egalDeplasareO3").value);
+	let puncteGazdeM1 = victorieAcasaG1 * 3 + egalAcasaG1 * 1 + victorieDeplasareG1 * 5 + egalDeplasareG1 * 2;
+	let puncteGazdeM2 = victorieAcasaG2 * 3 + egalAcasaG2 * 1 + victorieDeplasareG2 * 5 + egalDeplasareG2 * 2;
+	let puncteGazdeM3 = victorieAcasaG3 * 3 + egalAcasaG3 * 1 + victorieDeplasareG3 * 5 + egalDeplasareG3 * 2;
+	let puncteOaspetiM1 = victorieAcasaO1 * 3 + egalAcasaO1 * 1 + victorieDeplasareO1 * 5 + egalDeplasareO1 * 2;
+	let puncteOaspetiM2 = victorieAcasaO2 * 3 + egalAcasaO2 * 1 + victorieDeplasareO2 * 5 + egalDeplasareO2 * 2;
+	let puncteOaspetiM3 = victorieAcasaO3 * 3 + egalAcasaO3 * 1 + victorieDeplasareO3 * 5 + egalDeplasareO3 * 2;
+	let puncteTotGMet2 = puncteGazdeM1 + puncteGazdeM2 + puncteGazdeM3;
+	let puncteTotOMet2 = puncteOaspetiM1 + puncteOaspetiM2 + puncteOaspetiM3;
+	let met2 = (puncteTotGMet2 - puncteTotOMet2) * 100;
+	let met2Proc = Math.round((met2 / 15) * 1e2 ) / 1e2;
+
+	// Metoda III
+	let puncteTotGMet3 = puncteGazdeM1 * 1 + puncteGazdeM2 * 2 + puncteGazdeM3 * 3;
+	let puncteTotOMet3 = puncteOaspetiM1 * 1 + puncteOaspetiM2 * 2 + puncteOaspetiM3 * 3;
+	let met3 = (puncteTotGMet3 - puncteTotOMet3) * 100;
+	let met3Proc = Math.round((met3 / 30) * 1e2 ) / 1e2;
+
+	// Calcul final
+	let mediaSanselor = (met1Proc + met2Proc + met3Proc) / 3;
+	let mediaSanselorProc = Math.round(mediaSanselor * 1e2 ) / 1e2;
+	let sanseG = 50 + mediaSanselorProc;
+	let sanseO = 50 - mediaSanselorProc;
+
+
+	alert(met1Proc);
+	alert(met2Proc);
+	alert(met3Proc);
+	alert(mediaSanselor);
+	alert(mediaSanselorProc);
 	alert("Gazdele au sanse de castig de " + sanseG + "%, iar oaspetii au sanse de castig de " + sanseO + "%.");
 }
